8b2e74c9f709789578371876fa152b7ea2406345
Stephan D Ramalho <sramalho@fordham.edu>
2023-06-30 17:16:49 -0400

    replaced config.json with config.js file that uses .env variables

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config({ path: '.env.local' })
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_TEST,
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  production: {
+    // Production environment variables should be set separately
+    username: process.env.DB_USERNAME_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_NAME_PROD,
+    host: process.env.DB_HOST_PROD,
+    dialect: 'postgres'
+  }
+}
