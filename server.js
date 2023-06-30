499e6faae1c3f1803c4501f3389e91625fe0a9fb
Stephan D Ramalho <sramalho@fordham.edu>
2023-06-30 17:21:19 -0400

    created server file

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const AppRouter = require('./routes/AppRouter')
+
+const PORT = process.env.PORT || 3001
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => res.json({ message: 'Server Works' }))
+app.use('/api', AppRouter)
+
+module.exports = app
