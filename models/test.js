ba08a210f6406ea4dfa6984c312865ccf54dfe19
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-02 11:04:49 -0400

    created association to User

--- models/test.js
+++ models/test.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Test.belongsTo(models.User, { foreignKey: 'userId' })
     }
   }
   Test.init(
