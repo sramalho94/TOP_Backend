1d43d919e49ea3ec418fc96f7cbb31dd73131637
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-02 14:28:22 -0400

    userId key is now integer and references users properly. imported encrypt and decrypt methods

--- models/test.js
+++ models/test.js
@@ -1,59 +1,67 @@
-"use strict";
-const { Model } = require("sequelize");
+'use strict'
+const { Model } = require('sequelize')
+const { encrypt, decrypt } = require('../middleware/cryptoUtils')
 module.exports = (sequelize, DataTypes) => {
-	class Test extends Model {
-		/**
-		 * Helper method for defining associations.
-		 * This method is not a part of Sequelize lifecycle.
-		 * The `models/index` file will call this method automatically.
-		 */
-		static associate(models) {
-			// define association here
-			Test.belongsTo(models.User, { foreignKey: "userId" });
-		}
-	}
-	Test.init(
-		{
-			userId: DataTypes.STRING,
-			result: DataTypes.BOOLEAN,
-			ZIP: {
-				type: DataTypes.STRING,
-				get() {
-					const rawValue = this.getDataValue("ZIP");
-					return decrypt(rawValue);
-				},
-				set(val) {
-					this.setDataValue("ZIP", encrypt(val));
-				},
-			},
-			gender: {
-				type: DataTypes.STRING,
-				get() {
-					const rawValue = this.getDataValue("gender");
-					return decrypt(rawValue);
-				},
-				set(val) {
-					this.setDataValue("gender", encrypt(val));
-				},
-			},
-			race: {
-				type: DataTypes.STRING,
-				get() {
-					const rawValue = this.getDataValue("race");
-					return decrypt(rawValue);
-				},
-				set(val) {
-					this.setDataValue("race", encrypt(val));
-				},
-			},
+  class Test extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Test.belongsTo(models.User, { foreignKey: 'userId' })
+    }
+  }
+  Test.init(
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      result: DataTypes.BOOLEAN,
+      ZIP: {
+        type: DataTypes.STRING,
+        get() {
+          const rawValue = this.getDataValue('ZIP')
+          return decrypt(rawValue)
+        },
+        set(val) {
+          this.setDataValue('ZIP', encrypt(val))
+        }
+      },
+      gender: {
+        type: DataTypes.STRING,
+        get() {
+          const rawValue = this.getDataValue('gender')
+          return decrypt(rawValue)
+        },
+        set(val) {
+          this.setDataValue('gender', encrypt(val))
+        }
+      },
+      race: {
+        type: DataTypes.STRING,
+        get() {
+          const rawValue = this.getDataValue('race')
+          return decrypt(rawValue)
+        },
+        set(val) {
+          this.setDataValue('race', encrypt(val))
+        }
+      },
 
-			ethnicity: DataTypes.STRING,
-		},
-		{
-			sequelize,
-			modelName: "Test",
-			tableName: "tests",
-		}
-	);
-	return Test;
-};
+      ethnicity: DataTypes.STRING
+    },
+    {
+      sequelize,
+      modelName: 'Test',
+      tableName: 'tests'
+    }
+  )
+  return Test
+}
