c4c494821841a050a27d7639e5d67e8a2e78a9c6
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-03 09:04:24 -0400

    added cascade onDelte to userId key

--- migrations/20230702144433-create-test.js
+++ migrations/20230702144433-create-test.js
@@ -11,6 +11,7 @@ module.exports = {
       },
       userId: {
         type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
         allowNull: true,
         references: {
           model: 'users',
