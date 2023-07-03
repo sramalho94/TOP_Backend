b6df01fa0063a94e90223b2ee3deb73e55497048
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-03 11:23:16 -0400

    created AuthRouter and importated to AppRouter

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,6 +1,8 @@
 const Router = require('express').Router()
 const UserRouter = require('./UserRouter')
+const AuthRouter = require('./AuthRouter')
 
+Router.use('/auth', AuthRouter)
 Router.use('/users', UserRouter)
 
 module.exports = Router
