1990f75ace7a9ca61af988f826b6338996941345
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-03 11:34:42 -0400

    fixed typo

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -2,26 +2,31 @@ const router = require('express').Router()
 const controller = require('../controllers/UserController')
 const middleware = require('../middleware')
 
-router.get('/',middleware.stripToken, middleware.verifyToken, controller.getAllUsers)
 router.get(
-    '/:id',
-    middleware.stripToken,
-    middleware.verifyToken,
-    controller.getUserById
+  '/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.getAllUsers
+)
+router.get(
+  '/:id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.getUserById
 )
 
 router.put(
-    '/:id',
-    midddleware.stripToken,
-    middleware.verifyToken,
-    controller.updateUser
+  '/:id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.updateUser
 )
 
 router.delete(
-    '/:id',
-    middleware.stripToken,
-    middleware.verifyToken,
-    controller.deleteUser
+  '/:id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.deleteUser
 )
 
 module.exports = router
