010b962da75a7c6de4b74081ffda85666059dbcb
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-03 13:23:40 -0400

    accounted for potential null values in encrypted keys

--- models/user.js
+++ models/user.js
@@ -4,106 +4,94 @@ const { encrypt, decrypt } = require('../middleware/cryptoUtils')
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
       User.hasMany(models.Test, { foreignKey: 'userId' })
     }
   }
+
   User.init(
     {
       username: {
         type: DataTypes.STRING,
-        get() {
-          const rawValue = this.getDataValue('username')
-          return decrypt(rawValue)
-        },
-        set(val) {
-          this.setDataValue('username', encrypt(val))
-        }, 
-        allowNull: false,      
+        allowNull: false
       },
       passwordDigest: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: false
       },
       DOB: {
         type: DataTypes.STRING,
         get() {
           const rawValue = this.getDataValue('DOB')
-          return decrypt(rawValue)
+          return rawValue ? decrypt(rawValue) : rawValue
         },
         set(val) {
           this.setDataValue('DOB', encrypt(val))
         },
-        allowNull: false,
+        allowNull: false
       },
       state: {
         type: DataTypes.STRING,
         get() {
           const rawValue = this.getDataValue('state')
-          return decrypt(rawValue)
+          return rawValue ? decrypt(rawValue) : rawValue
         },
         set(val) {
           this.setDataValue('state', encrypt(val))
         },
-        allowNull: false,
+        allowNull: false
       },
       ZIP: {
         type: DataTypes.STRING,
         get() {
           const rawValue = this.getDataValue('ZIP')
-          return decrypt(rawValue)
+          return rawValue ? decrypt(rawValue) : rawValue
         },
         set(val) {
           this.setDataValue('ZIP', encrypt(val))
         },
-        allowNull: false,
+        allowNull: false
       },
       firstName: {
         type: DataTypes.STRING,
         get() {
           const rawValue = this.getDataValue('firstName')
-          return decrypt(rawValue)
+          return rawValue ? decrypt(rawValue) : rawValue
         },
         set(val) {
           this.setDataValue('firstName', encrypt(val))
-        },
+        }
       },
       gender: {
         type: DataTypes.STRING,
         get() {
           const rawValue = this.getDataValue('gender')
-          return decrypt(rawValue)
+          return rawValue ? decrypt(rawValue) : rawValue
         },
         set(val) {
           this.setDataValue('gender', encrypt(val))
-        },
+        }
       },
       ethnicity: {
         type: DataTypes.STRING,
         get() {
           const rawValue = this.getDataValue('ethnicity')
-          return decrypt(rawValue)
+          return rawValue ? decrypt(rawValue) : rawValue
         },
         set(val) {
           this.setDataValue('ethnicity', encrypt(val))
-        },
+        }
       },
-      race: { 
+      race: {
         type: DataTypes.STRING,
         get() {
           const rawValue = this.getDataValue('race')
-          return decrypt(rawValue)
+          return rawValue ? decrypt(rawValue) : rawValue
         },
         set(val) {
           this.setDataValue('race', encrypt(val))
-        },
-      },
+        }
+      }
     },
     {
       sequelize,
@@ -111,5 +99,6 @@ module.exports = (sequelize, DataTypes) => {
       tableName: 'users'
     }
   )
+
   return User
 }
