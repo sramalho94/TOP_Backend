fa7f5fffea148a2947cb758404c522e89c9e63eb
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-03 11:39:45 -0400

    fixed typos

--- routes/TestRouter.js
+++ routes/TestRouter.js
@@ -1,37 +1,37 @@
-const router = require("express").Router();
-const { verify } = require("jsonwebtoken");
-const controller = require("../controllers/TestController");
-const middleware = require("../middleware");
+const router = require('express').Router()
+const { verify } = require('jsonwebtoken')
+const controller = require('../controllers/TestController')
+const middleware = require('../middleware')
 
 router.get(
-  "/",
+  '/',
   middleware.stripToken,
   middleware.verifyToken,
-  controller.getAllTests
-);
+  controller.getAllTest
+)
 router.get(
-  "/:id",
+  '/:id',
   middleware.stripToken,
   middleware.verifyToken,
   controller.getTestById
-);
+)
 router.post(
-  "/",
+  '/',
   middleware.stripToken,
   middleware.verifyToken,
   controller.createTest
-);
+)
 router.put(
-  "/:id",
+  '/:id',
   middleware.stripToken,
   middleware.verifyToken,
   controller.updateTest
-);
+)
 router.delete(
-  "/:id",
+  '/:id',
   middleware.stripToken,
   middleware.verifyToken,
   controller.deleteTest
-);
+)
 
-module.exports = router;
+module.exports = router
