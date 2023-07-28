56cf47237305ec1ee91df05c5e5c4aa39ad08c4f
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-28 16:07:17 -0400

    refactoring to include new key for User

--- test/AuthController.test.js
+++ test/AuthController.test.js
@@ -1,90 +1,89 @@
 process.env.NODE_ENV = 'test'
 const request = require('supertest')
 const app = require('../server')
-const {createToken, hashPassword} = require('../middleware')
+const { createToken, hashPassword } = require('../middleware')
 const { User } = require('../models')
 
 describe('Auth controller test', () => {
-    let testUser
-    let testToken
-    
-    test('Create user', async () => {
+  let testUser
+  let testToken
 
-        const response = await request(app).post('/api/auth/register').send({
-            username: 'testuser',
-            password: 'testpassword',
-            DOB: '1999-05-01',
-            state: 'userstate',
-            ZIP: 'userZIP',
-            firstName: 'userfirstname',
-            gender: 'usergender',
-            ethnicity: 'userethnicity',
-            race: 'userrace'
-        })
-
-        testUser = response.body
-
-        expect(response.statusCode).toBe(201)
-        expect(testUser.username).toBe('testuser')
-        expect(testUser.id).toBe(testUser.id)
+  test('Create user', async () => {
+    const response = await request(app).post('/api/auth/register').send({
+      username: 'testuser',
+      password: 'testpassword',
+      DOB: '1999-05-01',
+      state: 'userstate',
+      ZIP: 'userZIP',
+      firstName: 'userfirstname',
+      gender: 'usergender',
+      ethnicity: 'userethnicity',
+      email: 'test@email.com',
+      race: 'userrace'
     })
 
-    test('Login user', async () => {
-        const response = await request(app).post('/api/auth/login').send({
-            username: 'testuser',
-            password: 'testpassword'
-        })
+    testUser = response.body
 
-        testToken = response.body.token
-        
-        expect(response.statusCode).toBe(200)
-        expect(response.body).toHaveProperty('token')
-        expect(response.body.user.username).toBe('testuser')
+    expect(response.statusCode).toBe(201)
+    expect(testUser.username).toBe('testuser')
+    expect(testUser.id).toBe(testUser.id)
+  })
+
+  test('Login user', async () => {
+    const response = await request(app).post('/api/auth/login').send({
+      username: 'testuser',
+      password: 'testpassword'
     })
 
-    test('Check session test', async () => {
+    testToken = response.body.token
 
-        const response = await request(app)
-            .get('/api/auth/session')
-            .set('Authorization', `Bearer ${testToken}`)
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toHaveProperty('token')
+    expect(response.body.user.username).toBe('testuser')
+  })
 
-        expect(response.statusCode).toBe(200)
-        expect(response).toHaveProperty('body')
-    })
+  test('Check session test', async () => {
+    const response = await request(app)
+      .get('/api/auth/session')
+      .set('Authorization', `Bearer ${testToken}`)
 
-    test('Update password', async () => {
-        const response = await request(app)
-            .put('/api/auth/update-password')
-            .set('Authorization', `Bearer ${testToken}`)
-            .send({
-                username: 'testuser',
-                oldPassword: 'testpassword',
-                newPassword: 'testnewpassword'
-            })
+    expect(response.statusCode).toBe(200)
+    expect(response).toHaveProperty('body')
+  })
 
-        expect(response.body.status).toBe('Success')
-        expect(response.body.msg).toBe('Password updated')
-    })
+  test('Update password', async () => {
+    const response = await request(app)
+      .put('/api/auth/update-password')
+      .set('Authorization', `Bearer ${testToken}`)
+      .send({
+        username: 'testuser',
+        oldPassword: 'testpassword',
+        newPassword: 'testnewpassword'
+      })
 
-    test('Update password fail', async () => {
-        const response = await request(app)
-            .put('/api/auth/update-password')
-            .set('Authorization', `Bearer ${testToken}`)
-            .send({
-                username: 'testuser',
-                oldPassword: 'testpassword1',
-                newPassword: 'testnewpassword'
-            })
+    expect(response.body.status).toBe('Success')
+    expect(response.body.msg).toBe('Password updated')
+  })
 
-            expect(response.body.status).toBe('Error')
-            expect(response.body.msg).toBe('Unauthorized')
-    })
+  test('Update password fail', async () => {
+    const response = await request(app)
+      .put('/api/auth/update-password')
+      .set('Authorization', `Bearer ${testToken}`)
+      .send({
+        username: 'testuser',
+        oldPassword: 'testpassword1',
+        newPassword: 'testnewpassword'
+      })
 
-    afterAll(async () => {
-        try{
-            await User.destroy({ truncate: { cascade: true } })
-        } catch (error) {
-            console.log('Error cleaning up test data: ', error)
-        }
-    })
+    expect(response.body.status).toBe('Error')
+    expect(response.body.msg).toBe('Unauthorized')
+  })
+
+  afterAll(async () => {
+    try {
+      await User.destroy({ truncate: { cascade: true } })
+    } catch (error) {
+      console.log('Error cleaning up test data: ', error)
+    }
+  })
 })
