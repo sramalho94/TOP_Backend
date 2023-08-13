3bf1bda810ae36e85d41e8f538674ba2f97a7828
Stephan D Ramalho <sramalho@fordham.edu>
2023-08-13 10:56:39 -0400

    removed comment from code, set rejectUnauthorized to true

--- config/config.js
+++ config/config.js
@@ -6,26 +6,28 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: '127.0.0.1',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_TEST,
     host: '127.0.0.1',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432
   },
   production: {
-    // Production environment variables should be set separately
     username: process.env.DB_USERNAME_PROD,
     password: process.env.DB_PASSWORD_PROD,
     database: process.env.DB_NAME_PROD,
     host: process.env.DB_HOST_PROD,
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false
+        rejectUnauthorized: true
       }
     }
   }
